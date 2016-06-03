f0530d53da5468c26cff9b8d86b546ace334cbf0
Christopher <chrisblupodev@gmail.com>
2016-06-03 00:38:51 +0000

    make reset button actually reset

--- script.js
+++ script.js
@@ -2,15 +2,20 @@ window.onload = function() {
     try {
         var store = localStorage.getItem('nums');
         store = JSON.parse(store);
-    } catch (e) {}
+    }
+    catch (e) {}
     var rslts = document.querySelector('ul');
     var minInput = document.querySelector('#min');
     var maxInput = document.querySelector('#max');
-    if (store !== null)
-    {
+    if (store !== null) {
         minInput.value = Number(store.minStore);
         maxInput.value = Number(store.maxStore);
     }
+
+    document.getElementById('reset').onclick = function() {
+        document.getElementById('list').innerHTML = '';
+    };
+
     document.onsubmit = function(e) {
         e.preventDefault();
         var min = Number(minInput.value);
@@ -20,12 +25,7 @@ window.onload = function() {
             maxStore: max
         }))
         var record = document.createElement('li');
-        record.textContent = Math.floor((Math.random()*(max-min))
-                          + Number(min));
+        record.textContent = Math.floor((Math.random() * (max - min)) + Number(min));
         rslts.insertBefore(record, document.querySelector('li:first-of-type'));
     }
-    $('#reset').click(function(e) {
-        e.preventDefault();
-        $('ul li').remove();
-    });
 };
